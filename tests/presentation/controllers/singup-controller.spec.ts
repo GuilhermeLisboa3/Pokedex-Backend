4f6c39584eea8838cdbc63473f8956b955cd78aa
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-11-30 23:44:00 -0300

    test: ensure SingUpController call AddAccount with correct values

--- tests/presentation/controllers/singup-controller.spec.ts
+++ tests/presentation/controllers/singup-controller.spec.ts
@@ -1,6 +1,6 @@
 import { SingUpController } from '@/presentation/controllers'
 import { badRequest } from '@/presentation/helpers'
-import { ValidationSpy } from '../mocks'
+import { ValidationSpy, AddAccountSpy } from '../mocks'
 import { accountParams } from '@/tests/mocks'
 
 const { name, email, password } = accountParams
@@ -13,28 +13,37 @@ const makeRequest = {
 type SutTypes = {
   sut: SingUpController
   validation: ValidationSpy
+  addAccountSpy: AddAccountSpy
 }
 
 const makeSut = (): SutTypes => {
   const validation = new ValidationSpy()
-  const sut = new SingUpController(validation)
+  const addAccountSpy = new AddAccountSpy()
+  const sut = new SingUpController(validation, addAccountSpy)
   return {
     sut,
-    validation
+    validation,
+    addAccountSpy
   }
 }
 
 describe('SingUp Controller', () => {
-  it('should call Validation with correct value ', () => {
+  it('should call Validation with correct value ', async () => {
     const { sut, validation } = makeSut()
-    sut.handle(makeRequest)
+    await sut.handle(makeRequest)
     expect(validation.input).toEqual(makeRequest)
   })
 
-  it('should return 400 if Validation returns in error', () => {
+  it('should return 400 if Validation returns in error', async () => {
     const { sut, validation } = makeSut()
     validation.error = new Error()
-    const httpResponse = sut.handle(makeRequest)
+    const httpResponse = await sut.handle(makeRequest)
     expect(httpResponse).toEqual(badRequest(validation.error))
+  })
+
+  it('should call AddAccount with correct value ', async () => {
+    const { sut, addAccountSpy } = makeSut()
+    await sut.handle(makeRequest)
+    expect(addAccountSpy.addAccountParams).toEqual(makeRequest)
   })
 })
