fd4d3a457d499f323d2b0035170fbe1b6774b783
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-01 22:07:09 -0300

    test: ensure ValidationComposite return an error if any validation fails

---
+++ tests/validations/validators/validation-composite.spec.ts
@@ -0,0 +1,31 @@
+import { ValidationSpy } from '@/tests/presentation/mocks'
+import { ValidationComposite } from '@/validations/validators'
+import { faker } from '@faker-js/faker'
+
+const field = faker.random.word()
+
+type SutTypes = {
+  sut: ValidationComposite
+  validationSpies: ValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpies = [
+    new ValidationSpy(),
+    new ValidationSpy()
+  ]
+  const sut = new ValidationComposite(validationSpies)
+  return {
+    sut,
+    validationSpies
+  }
+}
+
+describe('Validation Composite', () => {
+  it('should return an error if any validation fails', () => {
+    const { sut, validationSpies } = makeSut()
+    validationSpies[1].error = new Error(field)
+    const error = sut.validate({ field })
+    expect(error).toEqual(validationSpies[1].error)
+  })
+})
