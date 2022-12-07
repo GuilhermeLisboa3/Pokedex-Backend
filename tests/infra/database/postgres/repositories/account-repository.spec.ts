0f1b2eed6730074b6195f5e5dff89c480e180b0b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-07 18:48:22 -0300

    test: ensure AccountRepository return true if email exist

---
+++ tests/infra/database/postgres/repositories/account-repository.spec.ts
@@ -0,0 +1,26 @@
+import { sequelize, Account } from '@/infra/database/postgres/entities'
+import { AccountRepository } from '@/infra/database/postgres/repositories'
+import { accountParams } from '@/tests/mocks'
+
+const { name, email, password } = accountParams
+
+const makeSut = (): AccountRepository => {
+  return new AccountRepository()
+}
+
+describe('Account Repository', () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true })
+  })
+  afterAll(async () => {
+    await sequelize.close()
+  })
+  describe('checkByEmail', () => {
+    it('should return true if email exist', async () => {
+      const sut = makeSut()
+      await Account.create({ name, email, password })
+      const account = await sut.checkByEmail(email)
+      expect(account).toBe(true)
+    })
+  })
+})
