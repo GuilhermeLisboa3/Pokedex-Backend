281a4d2d1db7b58d2493844c0d20e134411953c5
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-06 21:06:03 -0300

    feat: ensure EmailValidatorAdapter return false if validator returns false

--- src/infra/validators/email-validator-adapter.ts
+++ src/infra/validators/email-validator-adapter.ts
@@ -2,7 +2,7 @@ import { EmailValidator } from '@/validations/protocols'
 import validator from 'validator'
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
-    validator.isEmail(email)
-    return null
+    const isValid = validator.isEmail(email)
+    return isValid
   }
 }
