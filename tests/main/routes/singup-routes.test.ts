8e0ee123eb70468109bc0a1dceaf44b41e92672a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-08 20:22:17 -0300

    refactor: connect to mongodb before integration tests

--- tests/main/routes/singup-routes.test.ts
+++ tests/main/routes/singup-routes.test.ts
@@ -1,10 +1,17 @@
 import request from 'supertest'
+import { sequelize } from '@/infra/database/postgres/entities'
 import app from '@/main/config/app'
 import { accountParams } from '@/tests/mocks'
 
 const { name, email, password } = accountParams
 
 describe('SignUp Routes', () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true })
+  })
+  afterAll(async () => {
+    await sequelize.close()
+  })
   it('should return an account on sucess ', async () => {
     await request(app)
       .post('/register')
