687f750afe38c66bd6ec9a3a6bef7834b8cdf67e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-08 13:58:12 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,6 @@
 import { Express } from 'express'
-import { bodyParser } from '../middlewares'
+import { bodyParser, cors } from '../middlewares'
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
