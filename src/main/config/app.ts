2af0fe0567008cd115754fc86e581f1b21a4ab2d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-08 15:08:45 -0300

    feat: ensure SingUp route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import setupMiddleware from './middlewares'
+import setupRoutes from './routes'
 
 const app = express()
 setupMiddleware(app)
+setupRoutes(app)
 
 export default app
---
+++ src/main/config/routes.ts
@@ -0,0 +1,11 @@
+import { Express, Router } from 'express'
+import { readdirSync } from 'fs'
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/', router)
+  readdirSync(`${String(__dirname)}/../routes`).map(async file => {
+    if (!file.endsWith('.map') && !file.includes('.test.')) {
+      (await import(`../routes/${file}`)).default(router)
+    }
+  })
+}
