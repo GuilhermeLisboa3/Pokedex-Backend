92a30e58ba00a4871c25cc2ecb673f7887843f95
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-07 22:54:09 -0300

    feat: ensure AccountRepository return an account on success

--- src/infra/database/postgres/repositories/account-repository.ts
+++ src/infra/database/postgres/repositories/account-repository.ts
@@ -1,13 +1,18 @@
-import { CheckAccountByEmailRepository } from '@/data/protocols'
+import { CheckAccountByEmailRepository, AddAccountRepository } from '@/data/protocols'
 import { Account } from '@/infra/database/postgres/entities'
 
-export class AccountRepository implements CheckAccountByEmailRepository {
+export class AccountRepository implements CheckAccountByEmailRepository, AddAccountRepository {
   async checkByEmail (email: string): Promise<boolean> {
     const account = await Account.findOne({
       where: {
         email
       }
     })
+    return account != null
+  }
+
+  async add (accountParams: AddAccountRepository.Params): Promise<boolean> {
+    const account = await Account.create(accountParams)
     return account != null
   }
 }
