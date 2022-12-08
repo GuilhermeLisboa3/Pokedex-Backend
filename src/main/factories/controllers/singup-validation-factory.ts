209c30e56f0efa4985adedc4dce6b3223699878e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-08 16:06:00 -0300

    feat: ensure makeSingUpValidation call ValidationComposite with all validations

---
+++ src/main/factories/controllers/singup-validation-factory.ts
@@ -0,0 +1,12 @@
+import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '@/validations/validators'
+import { Validation } from '@/presentation/protocols'
+import { EmailValidatorAdapter } from '@/infra/validators'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['email', 'name', 'password']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+  return new ValidationComposite(validations)
+}
