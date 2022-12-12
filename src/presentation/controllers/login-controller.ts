c969b3d6968f5ba3b1f7b11f1a09725abeb6ed0d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-12 15:01:05 -0300

    feat: ensure LoginController return 500 if Authentication returns throws

--- src/presentation/controllers/login-controller.ts
+++ src/presentation/controllers/login-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
-import { badRequest, unauthorized } from '@/presentation/helpers'
+import { badRequest, serverError, unauthorized } from '@/presentation/helpers'
 import { Authentication } from '@/domain/usecases'
 
 export class LoginController implements Controller {
@@ -9,14 +9,18 @@ export class LoginController implements Controller {
   ) {}
 
   async handle (request: LoginController.Request): Promise<HttpResponse> {
-    const error = this.validation.validate(request)
-    if (error) {
-      return badRequest(error)
-    }
-    const { email, password } = request
-    const authenticationModel = await this.authentication.auth({ email, password })
-    if (!authenticationModel) {
-      return unauthorized()
+    try {
+      const error = this.validation.validate(request)
+      if (error) {
+        return badRequest(error)
+      }
+      const { email, password } = request
+      const authenticationModel = await this.authentication.auth({ email, password })
+      if (!authenticationModel) {
+        return unauthorized()
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
