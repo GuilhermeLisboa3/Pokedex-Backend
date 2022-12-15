7467a27dbefa146a5f596f13f21051eed2bbbe01
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-14 21:53:48 -0300

    feat: ensure JwtAdapter call sign with correct value

---
+++ src/infra/criptography/jwt-adapter.ts
@@ -0,0 +1,14 @@
+import { Encrypter } from '@/data/protocols'
+import jwt from 'jsonwebtoken'
+
+export class JwtAdapter implements Encrypter {
+  constructor (
+    private readonly secret: string,
+    private readonly expires: string
+  ) {}
+
+  async encrypt (plaintext: string): Promise<string> {
+    await jwt.sign(plaintext, this.secret, { expiresIn: this.expires })
+    return null
+  }
+}
