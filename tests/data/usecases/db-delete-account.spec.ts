d5d51b19567344947f0e6e1602597b96ac1b7dbd
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-16 19:41:12 -0300

    test: ensure DbDeleteAccount return error if checkById returns false

--- tests/data/usecases/db-delete-account.spec.ts
+++ tests/data/usecases/db-delete-account.spec.ts
@@ -25,4 +25,11 @@ describe('DbDeleteAccount', () => {
     await sut.delete(id)
     expect(checkByIdRepositorySpy.id).toBe(id)
   })
+
+  it('should return error if checkById returns false', async () => {
+    const { sut, checkByIdRepositorySpy } = makeSut()
+    checkByIdRepositorySpy.result = false
+    const error = await sut.delete(id)
+    expect(error).toEqual(new Error('The received id does not exists'))
+  })
 })
