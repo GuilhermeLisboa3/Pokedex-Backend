53f9d920a272141e14a2c031fb8eb32c21ef2e3d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-16 22:52:33 -0300

    feat: ensure DbDeleteAccount call DeleteByIdRepository with correct id

--- src/data/usecase/db-delete-account.ts
+++ src/data/usecase/db-delete-account.ts
@@ -1,14 +1,16 @@
 import { DeleteById } from '@/domain/usecases'
-import { CheckByIdRepository } from '@/data/protocols'
+import { CheckByIdRepository, DeleteByIdRepository } from '@/data/protocols'
 
 export class DbDeleteAccount implements DeleteById {
   constructor (
-    private readonly checkByIdRepository: CheckByIdRepository
+    private readonly checkByIdRepository: CheckByIdRepository,
+    private readonly deleteByIdRepository: DeleteByIdRepository
   ) {}
 
   async delete (id: string): Promise<boolean> {
     const exists = await this.checkByIdRepository.checkById(id)
     if (!exists) return null
+    await this.deleteByIdRepository.deleteById(id)
     return null
   }
 }
