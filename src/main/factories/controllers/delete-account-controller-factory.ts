5e68d9d723177c6cbc9ee5b5103cfca796370953
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-19 22:39:07 -0300

    feat: ensure DeleteAccount route delete an account on success

---
+++ src/main/factories/controllers/delete-account-controller-factory.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@/presentation/protocols'
+import { DeleteAccountController } from '@/presentation/controllers'
+import { DbDeleteAccount } from '@/data/usecase/db-delete-account'
+import { AccountRepository } from '@/infra/database/postgres/repositories'
+
+export const makeDeleteAccountController = (): Controller => {
+  const accountRepository = new AccountRepository()
+  const dbDeleteAccount = new DbDeleteAccount(accountRepository, accountRepository)
+  return new DeleteAccountController(dbDeleteAccount)
+}
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
-import { makeSingUpController, makeLoginController } from '@/main/factories/controllers'
+import { makeSingUpController, makeLoginController, makeDeleteAccountController } from '@/main/factories/controllers'
 
 export default (router: Router): void => {
   router.post('/register', adaptRoute(makeSingUpController()))
   router.post('/login', adaptRoute(makeLoginController()))
+  router.delete('/user', adaptRoute(makeDeleteAccountController()))
 }
