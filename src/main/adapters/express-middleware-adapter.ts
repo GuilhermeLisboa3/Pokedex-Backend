d511e09a123a21ab803bf65b60a88959ee3273f9
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-24 16:35:21 -0300

    feat: create express Middleware adapter

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,19 @@
+import { Middleware } from '../../presentation/protocols'
+import { Request, Response, NextFunction } from 'express'
+export const adaptMiddleware = (middleware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const request = {
+      ...(req.body || {}),
+      ...(req.headers || {})
+    }
+    const httpResponse = await middleware.handle(request)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, { id: httpResponse.body })
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
