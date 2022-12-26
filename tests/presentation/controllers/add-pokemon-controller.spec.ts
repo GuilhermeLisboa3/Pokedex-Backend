d5849a7ef263a4b9d0eccd2c57320a7d167fc9f7
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-26 13:55:38 -0300

    test: ensure AddPokemonController return 400 if Validation returns error

--- tests/presentation/controllers/add-pokemon-controller.spec.ts
+++ tests/presentation/controllers/add-pokemon-controller.spec.ts
@@ -1,6 +1,7 @@
 import { pokemonParams } from '@/tests/mocks'
 import { ValidationSpy } from '@/tests/presentation/mocks'
 import { AddPokemonController } from '@/presentation/controllers'
+import { badRequest } from '@/presentation/helpers'
 
 const makeRequest = {
   namePokemon: pokemonParams.namePokemon,
@@ -30,5 +31,12 @@ describe('AddPokemon Controller', () => {
     const { sut, validationSpy } = makeSut()
     await sut.handle(makeRequest)
     expect(validationSpy.input).toEqual(makeRequest)
+  })
+
+  it('should return 400 if Validation returns in error', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new Error()
+    const httpResponse = await sut.handle(makeRequest)
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
   })
 })
