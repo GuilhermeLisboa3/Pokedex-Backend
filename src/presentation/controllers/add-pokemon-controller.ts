29f38e293d518155fc9a58376f74fc615ba0867a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-26 14:02:52 -0300

    feat: add namespace in AddPokemonController

--- src/presentation/controllers/add-pokemon-controller.ts
+++ src/presentation/controllers/add-pokemon-controller.ts
@@ -6,11 +6,22 @@ export class AddPokemonController implements Controller {
     private readonly validation: Validation
   ) {}
 
-  async handle (request: any): Promise<HttpResponse> {
+  async handle (request: AddPokemonController.Request): Promise<HttpResponse> {
     const error = await this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
     return null
+  }
+}
+
+export namespace AddPokemonController {
+  export type Request = {
+    namePokemon: string
+    photoPokemon: string
+    idPokemon: number
+    types: string[]
+    urlSpecies: string
+    IdUser: number
   }
 }
