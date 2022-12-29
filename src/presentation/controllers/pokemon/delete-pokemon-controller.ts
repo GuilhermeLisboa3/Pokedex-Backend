ff11ed658a5d3dbfc1ce6bc66ce1a97c6e7552f4
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-29 16:41:27 -0300

    feat: ensure DeletePokemonController return 500 if DeletePokemon returns throws

--- src/presentation/controllers/pokemon/delete-pokemon-controller.ts
+++ src/presentation/controllers/pokemon/delete-pokemon-controller.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpResponse } from '@/presentation/protocols'
 import { DeletePokemon } from '@/domain/usecases'
-import { badRequest } from '@/presentation/helpers'
+import { badRequest, serverError } from '@/presentation/helpers'
 import { NonExistentFieldError } from '@/presentation/errors'
 
 export class DeletePokemonController implements Controller {
@@ -9,10 +9,14 @@ export class DeletePokemonController implements Controller {
   ) {}
 
   async handle (request: any): Promise<HttpResponse> {
-    const deletePokemon = await this.deletePokemon.delete(request.id)
-    if (!deletePokemon) {
-      return badRequest(new NonExistentFieldError('idPokemon'))
+    try {
+      const deletePokemon = await this.deletePokemon.delete(request.id)
+      if (!deletePokemon) {
+        return badRequest(new NonExistentFieldError('idPokemon'))
+      }
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    return null
   }
 }
