5b9d23af28809fd34557de7f020cecc48d163a95
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-29 14:34:06 -0300

    feat: ensure PokemonRepository return list pokemons on success

--- src/infra/database/postgres/repositories/pokemon-repository.ts
+++ src/infra/database/postgres/repositories/pokemon-repository.ts
@@ -1,7 +1,7 @@
-import { CheckPokemonRepository, AddPokemonRepository } from '@/data/protocols'
+import { CheckPokemonRepository, AddPokemonRepository, ListPokemonsRepository } from '@/data/protocols'
 import { Pokemon } from '@/infra/database/postgres/entities'
 
-export class PokemonRepository implements CheckPokemonRepository, AddPokemonRepository {
+export class PokemonRepository implements CheckPokemonRepository, AddPokemonRepository, ListPokemonsRepository {
   async checkPokemon (namePokemon: string, accountId: number): Promise<boolean> {
     const pokemon = await Pokemon.findOne({
       where: {
@@ -15,5 +15,21 @@ export class PokemonRepository implements CheckPokemonRepository, AddPokemonRepo
   async add (pokemonParams: AddPokemonRepository.Params): Promise<boolean> {
     const pokemon = await Pokemon.create({ ...pokemonParams, userId: pokemonParams.accountId })
     return pokemon != null
+  }
+
+  async list (accountId: number): Promise<ListPokemonsRepository.Result> {
+    const pokemons = await Pokemon.findAll({
+      attributes: [
+        'id',
+        ['id_pokemon', 'idPokemon'],
+        ['name_pokemon', 'namePokemon'],
+        ['photo_pokemon', 'photoPokemon'],
+        'types',
+        ['url_species', 'urlSpecies']
+      ],
+      where: { userId: accountId }
+    })
+    console.log(pokemons)
+    return pokemons
   }
 }
