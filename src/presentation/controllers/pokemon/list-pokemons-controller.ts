42e38411ec4642e6d997494f4c7ae39813feaac0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-28 23:15:07 -0300

    feat: ensure ListPokemonController return list pokemons on success

--- src/presentation/controllers/pokemon/list-pokemons-controller.ts
+++ src/presentation/controllers/pokemon/list-pokemons-controller.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpResponse } from '@/presentation/protocols'
 import { ListPokemons } from '@/domain/usecases'
+import { ok } from '@/presentation/helpers'
 
 export class ListPokemonsController implements Controller {
   constructor (
@@ -8,8 +9,8 @@ export class ListPokemonsController implements Controller {
 
   async handle (request: ListPokemonsController.Request): Promise<HttpResponse> {
     const { accountId } = request
-    await this.listPokemons.list(accountId)
-    return null
+    const pokemons = await this.listPokemons.list(accountId)
+    return ok(pokemons)
   }
 }
 
