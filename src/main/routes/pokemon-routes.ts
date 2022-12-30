218d0fb018bdf1fc37967d7f4773ea1d70b7d935
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-30 01:45:19 -0300

    feat: ensure route delete pokemon return 403 if acceddToken is not supplied

--- src/main/routes/pokemon-routes.ts
+++ src/main/routes/pokemon-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { adaptRoute, adaptMiddleware } from '@/main/adapters'
-import { makeAddPokemonController, makeListPokemonsController } from '@/main/factories/controllers'
+import { makeAddPokemonController, makeListPokemonsController, makeDeletePokemonController } from '@/main/factories/controllers'
 import { makeAuthMiddleware } from '@/main/factories/middlewares'
 
 export default (router: Router): void => {
   router.post('/pokemon', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeAddPokemonController()))
   router.get('/pokemons', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeListPokemonsController()))
+  router.delete('/pokemon/:id', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeDeletePokemonController()))
 }
