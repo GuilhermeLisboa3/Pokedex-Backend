a305748ffbf76c74150f23ad0c1b67a967210161
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-12-31 14:47:43 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setupMiddleware from './middlewares'
+import setupSwagger from './config-swagger'
 import setupRoutes from './routes'
 
 const app = express()
+setupSwagger(app)
 setupMiddleware(app)
 setupRoutes(app)
 
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Pokedex Pokemon Node',
+    description: 'API de uma pokedex, que serve para criar um usuario e salvar seus pokemons na sua conta.',
+    version: '1.0.0'
+  }
+}
